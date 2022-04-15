bdaac345122c1557900038067495366a04fd237c
disyled25 <nazarow.george@gmail.com>
2022-04-15 19:02:24 +0300

    Create lab 5.12.fs

---
+++ Lab 5/lab 5.12.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+[<EntryPoint>]
+
+let main args = 
+    let question user_answer =
+        match user_answer with
+            | "F#" | "Prolog" -> "Sycophant"
+            | "C++" | "Python" -> "Kinda basic"
+            | "C" | "Pascal" -> "You're a dinosaur"
+            | "Java" -> "Do you make mods for Minecraft?"
+            | "C#" -> "Unity developer right here"
+            | "Rust" | "Perl" | "Ruby" -> "Are you even exist?"
+            | "JavaScript" -> "Are you misspelled Java?"
+            | others -> "Didn't expect that"
+
+    // superposition
+    Console.WriteLine("What is your favorite programming language?")
+    (Console.ReadLine >> question >> printfn "%s") ()
+
+    // currying
+    Console.WriteLine("What is your favorite programming language?")
+    let answer input (output:string -> unit) choose = output(choose(input()))
+    answer Console.ReadLine Console.WriteLine question
+    0
