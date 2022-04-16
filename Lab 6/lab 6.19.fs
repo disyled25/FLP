6ff8f8a14f4ff589d9423f895cbea6ea34f06a00
disyled25 <nazarow.george@gmail.com>
2022-04-17 02:30:23 +0300

    Create lab 6.19.fs

---
+++ Lab 6/lab 6.19.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+// Function that allows the user to enter a list from the console
+let list_input =
+    let rec l_i list =
+        let input = Convert.ToInt32(Console.ReadLine())
+        if input = 0 then list
+        else l_i (list@[input])
+    l_i []
+
+let list_positive_negative_order list = 
+    let rec positive list final_list= 
+        match list with
+        |H::T -> if H > 0 then positive T (final_list@[H]) else positive T final_list
+        |[] -> final_list
+
+    let rec negative list final_list = 
+        match list with
+        |H::T -> if H < 0 then negative T (final_list@[H]) else negative T final_list
+        |[] -> final_list
+
+    (positive list [])@(negative list [])
+
+[<EntryPoint>]
+let main args = 
+    printfn "Minimal even number is %A" (list_positive_negative_order list_input)
+    0
