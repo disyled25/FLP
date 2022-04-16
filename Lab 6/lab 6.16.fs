61cb04f5c58d63b288844fae326b685f14f386f6
disyled25 <nazarow.george@gmail.com>
2022-04-17 00:40:48 +0300

    Create lab 6.16.fs

---
+++ Lab 6/lab 6.16.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+// Function that allows the user to enter a list from the console
+let list_input =
+    let rec l_i list =
+        let input = Convert.ToInt32(Console.ReadLine())
+        if input = 0 then list
+        else l_i (list@[input])
+    l_i []
+
+// Count all even numbers in given list
+let count_even_in_list list = 
+    let rec count_even list count =
+        match list with
+        |H::T -> if H % 2 = 0 then count_even T (count + 1) else count_even T count
+        |[] -> count
+    count_even list 0
+
+
+[<EntryPoint>]
+let main args = 
+    printfn "\nThere is %A even numbers in list" (count_even_in_list list_input)
+    0
