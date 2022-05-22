49445c8e3800b1efa0772f135630a09345ecc57a
disyled25 <nazarow.george@gmail.com>
2022-05-22 10:35:25 +0300

    Update lab 11.15.pl

--- Prolog/Lab 11/lab 11.15.pl
+++ Prolog/Lab 11/lab 11.15.pl
@@ -1,5 +1,5 @@
-digits_mult_up(0,N,N):-!.
-digits_mult_up(X,N,Ini):-Temp is X mod 10, Ini1 is Ini * Temp, X1 is X div 10, digits_mult_up(X1,N,Ini1).
-digits_mult_up(X):-digits_mult_up(X,N,1), write(N).
+digits_mult_up(0,1):-!.
+digits_mult_up(X,Y):-K is X mod 10, X1 is X div 10, digits_mult_up(X1,K1), Y is K*K1.
+
 
 
