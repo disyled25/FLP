47ab64598407474bb5070db34719b78f534cbcc6
disyled25 <nazarow.george@gmail.com>
2022-06-03 23:48:27 +0300

    Update lab 12.pl

--- Prolog/lab 12.pl
+++ Prolog/lab 12.pl
@@ -21,12 +21,27 @@ r_l([],List,List):-!.
 r_l([H|T],Blank,FinalList):- r_l(T,[H|Blank],FinalList).
 reversed_list([H|T],FinalList):- r_l([H|T],[],FinalList),!.
 
-%Returns the minimal element of the given list
-min_in_l([],CurMin,CurMin):-!.
-min_in_l([H|T],CurMin,Min):- H < CurMin, C is H, min_in_l(T,C,Min).
-min_in_l([_|T],CurMin,Min):- min_in_l(T,CurMin,Min).
-min_in_list([H|T],Min):-CurMin is H, min_in_l([H|T],CurMin,Min),!.
+%Returns the minimal element of the given list and it's index
+min_in_l([],CurMin,CurMin,_,I,I):-!.
+min_in_l([H|T],CurMin,Min,I1,_,I):- H < CurMin, C is H, I2 is I1 + 1,
+    Temp1 is I2, min_in_l(T,C,Min,I2,Temp1,I).
+min_in_l([_|T],CurMin,Min,I1,Temp,I):- I2 is I1 + 1, min_in_l(T,CurMin,Min,I2,Temp,I).
+min_in_list([H|T],Min,I):-CurMin is H, min_in_l(T,CurMin,Min,1,_,I),!.
 
+%Returns the maximal element of the given list and it's index
+max_in_l([],CurMax,CurMax,_,I,I):-!.
+max_in_l([H|T],CurMax,Max,I1,_,I):- H > CurMax, C is H, I2 is I1 + 1,
+    Temp1 is I2, max_in_l(T,C,Max,I2,Temp1,I).
+max_in_l([_|T],CurMax,Max,I1,Temp,I):- I2 is I1 + 1, max_in_l(T,CurMax,Max,I2,Temp,I).
+max_in_list([H|T],Max,I):-CurMax is H, max_in_l(T,CurMax,Max,1,_,I),!.
+
+l_in_i([],List,_,_,_,List):-!.
+l_in_i([H|T],List1,I1,I2,Icur,Ans):- Icur > I1, Icur < I2, Cur is Icur + 1,
+    l_in_i(T,[H|List1],I1,I2,Cur,Ans).
+l_in_i([_|T],List1,I1,I2,Icur,Ans):-Cur is Icur + 1, l_in_i(T,List1,I1,I2,Cur,Ans).
+list_in_interval(List,I1,I2,Ans):- l_in_i(List,[],I1,I2,1,Ans1), reversed_list(Ans1,Ans),!.
+
+%----------------------------------------------------------------------
 
 %Exercise 11 (up)
 el_up(_,1,0):-!.
@@ -77,11 +92,8 @@ in_number_exclude(N,X,N1,I):- N > 10,
     I1 is I div 10,
     in_number_exclude(Ncur,X,N1cur,I1),
     X1div is X1 div 10, N1 is X1div + N1cur.
-in_number_exclude(N,X,N1,I):-
-    Num is N div I, Num > 10, I1 is I * 10,
-    in_number_exclude(N,X,N1,I1).
 
-in_number_exclude(Number,Digit,New_number):- in_number_exclude(Number,Digit,New_number,1).
+in_number_exclude(Number,Digit,New_number):- num_class(Number,I), in_number_exclude(Number,Digit,New_number,I).
 
 perm_num(X,X,1):- !.
 perm_num(Number,Permutation,I):-
@@ -102,5 +114,9 @@ list_lenght(List,Ans):- l_l(List,0,Ans).
 %Exercise 15 (1.9)
 ft([H|T],H,T):-!.
 ft([H|T],Min,Ans):-H \= Min, ft(T,Min,Ans).
-fithteen(List,Ans):- reversed_list(List,List1), min_in_list(List1,Min),
+fifteen(List,Ans):- reversed_list(List,List1), min_in_list(List1,Min,_),
     ft(List1,Min,List2),reversed_list(List2,Ans),!.
+
+%Exercise 16 (1.12)
+sixteen(List,Ans):- min_in_list(List,_,I), max_in_list(List,_, I1),
+    (I<I1,list_in_interval(List,I,I1,Ans);list_in_interval(List,I1,I,Ans)),!.
