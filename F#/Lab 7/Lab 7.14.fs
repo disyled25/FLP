3ec4de5271ebce0b08c7f6bc8651a54c32852eab
disyled25 <nazarow.george@gmail.com>
2022-06-04 05:56:31 +0300

    Create Lab 7.14.fs

---
+++ F#/Lab 7/Lab 7.14.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+let MinPrime list = 
+    List.min (List.filter (fun x -> x % 2 = 0) list)
+
+[<EntryPoint>]
+let main args = 
+    printfn "%A" (MinPrime [1;3;5;9;8;20;12;4;5])
+    0
