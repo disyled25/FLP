7a667eb16505ec88b0ab51b6206d4ec919cb086c
disyled25 <nazarow.george@gmail.com>
2022-06-04 05:46:13 +0300

    Create Lab 7.13.fs

---
+++ F#/Lab 7/Lab 7.13.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+let IsLocalMax list index= 
+    let Max = List.item index list
+    if  (List.item (index - 1) list < Max) && (List.item (index + 1) list < Max) then true else false
+
+[<EntryPoint>]
+let main args = 
+    printfn "%A" (IsLocalMax [1;3;5;9;8;20;12;4] 3)
+    0
