667e7d2dd4504acf0da5a78032282b9671ff0c78
disyled25 <nazarow.george@gmail.com>
2022-06-04 04:40:47 +0300

    Create Lab 7.11.fs

---
+++ F#/Lab 7/Lab 7.11.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let rec f list1 list2 count = 
+    match list1 with
+        |H::T -> if List.contains H list2 then f T list2 (count + 1) else f T list2 count
+        |[] -> count
+
+[<EntryPoint>]
+let main args =
+    let list1 = [1;5;2;4;6;7;2]
+    let list2 = [1;4;6;9;0;11;3]
+    printf "%A" (f list1 list2 0)
+    0
